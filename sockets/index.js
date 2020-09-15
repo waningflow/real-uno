da797451fd862c7fd2946a6b904f05b406b46bd2
waningflow <jack1995woods@gmail.com>
2020-09-15 09:32:17 +0800

    feat: update user info

--- sockets/index.js
+++ sockets/index.js
@@ -4,38 +4,38 @@ const { joinRoom, findRoom, getRoomData } = require('./room');
 
 const init = (io) => {
   io.on('connection', (socket) => {
-    socket.on('create_room', ({ nickName }) => {
+    socket.on('create_room', ({ userInfo }) => {
       const roomId = genId(5);
       if (findRoom(roomId)) {
         socket.emit('create_room_result', { code: 1, message: 'fail' });
         return;
       }
       socket.join(roomId);
-      addSocket(socket, { nickName, roomId });
+      addSocket(socket, { userInfo, roomId });
       joinRoom(socket, roomId);
       const roomData = getRoomData(roomId);
       socket.emit('create_room_result', { code: 0, roomId, roomData });
-      console.log('create room:', roomId, 'nickName:', nickName);
+      console.log('create room:', roomId, 'nickName:', userInfo.nickName);
     });
-    socket.on('join_room', ({ roomId, nickName }) => {
+    socket.on('join_room', ({ roomId, userInfo }) => {
       if (!findRoom(roomId)) {
         socket.emit('join_room_result', { code: 1, message: 'not fount' });
         return;
       }
       socket.join(roomId);
-      addSocket(socket, { nickName, roomId });
+      addSocket(socket, { userInfo, roomId });
       joinRoom(socket, roomId);
       const roomData = getRoomData(roomId);
       socket.emit('join_room_result', { code: 0, roomId, roomData });
       socket.to(roomId).emit('update_room', { code: 0, roomData });
-      console.log('join room:', roomId, 'nickName:', nickName);
+      console.log('join room:', roomId, 'nickName:', userInfo.nickName);
     });
     socket.on('message', (data) => {
-      const { nickName, roomId } = getSocket(socket);
+      const { userInfo, roomId } = getSocket(socket);
       const { msg } = data;
       const msgId = genId(10);
-      io.in(roomId).send({ nickName, roomId, msg, msgId });
-      console.log('message come', 'roomId:', roomId, 'nickName:', nickName, 'msg:', msg);
+      io.in(roomId).send({ userInfo, roomId, msg, msgId });
+      console.log('message come', 'roomId:', roomId, 'nickName:', userInfo.nickName, 'msg:', msg);
     });
   });
 };
